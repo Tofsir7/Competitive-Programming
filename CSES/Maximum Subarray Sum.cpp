fdab0550ef9a1a9bed1adcc4ff4cc3b0e2d3fa0d
Khayrul Hassan Khan <khtofsir9191@gmail.com>
2025-04-23 23:26:42 +0600

    CSES

--- CSES/Maximum Subarray Sum.cpp
+++ CSES/Maximum Subarray Sum.cpp
@@ -10,32 +10,14 @@ int main()
 
 	int n;
 	cin>>n;
-	vector<int>numbers(n);
+	vector<long long int>numbers(n);
 	for(int i=0;i<n;i++)
 	cin>>numbers[i];
-	long long int sum=0,maxSum=INT_MIN;
-	for(int i=0;i<n;i++)
+	long long int sum=numbers[0],maxSum=numbers[0];
+	for(int i=1;i<n;i++)
 	{
-//		if(sum+numbers[i]<0)
-//		{
-//			maxSum=sum;
-//			i++;
-//			sum=numbers[i];
-//		}
-//		else
-//		{
-//			sum+=numbers[i];
-//			maxSum=max(sum,maxSum);
-//		}
-		long long int x=numbers[i];
-		maxSum=max(maxSum,x);
-		sum+=numbers[i];
-		if(sum<0)
-		{
-			sum=0;
-		}
-		else
-		maxSum=max(maxSum,sum);
+		sum=max(sum+numbers[i],numbers[i]);
+		maxSum=max(maxSum, sum);	
 	}
 	cout<<maxSum<<endl;
 }
